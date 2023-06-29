21e1b4b7627aad134bd6ba433a55ef1aedcca44a
Marko Budimir <marko.budimir12@gmail.com>
2023-06-29 10:55:38 +0200

    Edit server and client to use tcp for communication

--- src/main/java/org/example/server/ChatServer.java
+++ src/main/java/org/example/server/ChatServer.java
@@ -1,59 +1,27 @@
 package org.example.server;
 
 import java.io.IOException;
-import java.net.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.ServerSocket;
 
 public class ChatServer {
-    private static final int PORT_NUMBER = 4445;
-    private static final List<InetAddress> users = new ArrayList<>();
 
     public static void main(String[] args) {
-        boolean listening = true;
-        byte[] buf;
 
-        try (DatagramSocket serverSocket = new DatagramSocket()) {
-            serverSocket.setBroadcast(true);
-            new BroadcastServer(serverSocket).start();
-            while (listening) {
-                buf = new byte[256];
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                serverSocket.receive(packet);
-                InetAddress address = packet.getAddress();
-                int port = packet.getPort();
-
-                int userId = users.indexOf(address);
-                if (userId == -1) {
-                    users.add(address);
-                    userId = users.indexOf(address);
-                }
-                buf = ("User " + (userId + 1) + ": " + new String(packet.getData(), 0, packet.getLength()))
-                        .getBytes();
-
-                packet = new DatagramPacket(buf, buf.length, address, port);
-                String received = new String(packet.getData(), 0, packet.getLength());
-                broadcast(received, serverSocket);
-            }
-        } catch (SocketException e) {
-            System.err.println("Could not open socket");
-            System.exit(-1);
-        } catch (IOException e) {
-            System.err.println("Could not receive packet");
+        if (args.length != 1) {
+            System.err.println("Usage: ChatServer <port number>");
             System.exit(-1);
         }
-    }
 
-    private static void broadcast(String message, DatagramSocket socket) {
-        try {
-            InetAddress address = InetAddress.getByName("255.255.255.255");
-            DatagramPacket packet = new DatagramPacket(message.getBytes(), message.getBytes().length, address, PORT_NUMBER);
-            socket.send(packet);
-        } catch (UnknownHostException e) {
-            System.err.println("Could not find host");
-            System.exit(-1);
+        int portNumber = Integer.parseInt(args[0]);
+        boolean listening = true;
+        new BroadcastServer(portNumber).start();
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            while (listening) {
+                new ChatServerThread(serverSocket.accept()).start();
+            }
         } catch (IOException e) {
-            System.err.println("Could not send packet");
+            System.out.println("Could not listen on port " + portNumber);
             System.exit(-1);
         }
     }
